ffe72068f04c20a38680e6cf8a829e821da95cfb
StaciAF <aaen.it19@gmail.com>
2021-06-23 01:50:21 +0000

    adds solution to task 4

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -15,3 +15,22 @@ interface printTeacherFunction {
 export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
        return `${firstName[0]}. ${lastName}`;
 }
+interface StudentConstructor {
+	  new(firstName: string, lastName: string): StudentClassInterface;
+}
+interface StudentClassInterface {
+	  firstName: string;
+	  lastName: string;
+	  workOnHomework(): string;
+	  displayName(): string;
+}
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+       firstName: string;
+       lastName: string;
+       constructor(firstName: string, lastName: string) {
+       this.firstName = firstName;
+       this.lastName = lastName;
+}
+workOnHomework() {return 'Currently working';}
+displayName() {return firstName; }
+}
