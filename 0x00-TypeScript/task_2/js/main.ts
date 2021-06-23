96eb8e8700762b6a39396f7488e109d5b8fcd903
StaciAF <aaen.it19@gmail.com>
2021-06-23 03:17:42 +0000

    adds solutions for tasks 6 & 7

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -23,3 +23,15 @@ export function createEmployee(salary: number | string): Director | Teacher {
        if (typeof salary === 'number' && salary < 500) return new Teacher();
        return new Director();
 }
+export function isDirector(employee: Director | Teacher): employee is Director {
+       return employee instanceof Director;
+}
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+       if (!isDirector(employee)) return employee.workTeacherTasks();
+       else return employee.workDirectorTasks();
+}
+export type Subjects = 'Math' | 'History';
+export function teachClass(todayClass: Subjects): string {
+       if (todayClass === 'Math') return 'Teaching Math';
+       else if (todayClass === 'History') return 'Teaching History';
+}
