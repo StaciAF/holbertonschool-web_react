4cd5967915bc7b657517c9bbc88679c5e59fd28e
StaciAF <aaen.it19@gmail.com>
2021-06-23 03:04:38 +0000

    updates strings

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -11,7 +11,7 @@ interface TeacherInterface {
 export class Director implements DirectorInterface {
       workFromHome(): string { return 'Working from home'; }
       getCoffeeBreak(): string { return 'Getting a coffee break'; }
-      workTeachersTasks(): string { return 'Getting to director tasks'; }
+      workDirectorTasks(): string { return 'Getting to director tasks'; }
 }
 
 export class Teacher implements TeacherInterface {
@@ -20,6 +20,6 @@ export class Teacher implements TeacherInterface {
       workTeacherTasks(): string { return 'Getting to work'; }
 }
 export function createEmployee(salary: number | string): Director | Teacher {
-       if(typeof salary === 'number' && salary < 500) return new Teacher;
-       return new Director;
+       if (typeof salary === 'number' && salary < 500) return new Teacher();
+       return new Director();
 }
