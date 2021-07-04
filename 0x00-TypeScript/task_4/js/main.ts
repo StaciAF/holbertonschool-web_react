585b15dfc2995132af077eac33552593c5b8340d
StaciAF <aaen.it19@gmail.com>
2021-07-04 17:18:48 +0000

    adds files for task_4, needs linting and completion

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -3,3 +3,17 @@
 /// <reference path='./subjects/React.ts' />
 /// <reference path='./subjects/Subject.ts' />
 /// <reference path='./subjects/Teacher.ts' />
+
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: SUbjects.React = new Subjects.React();
+
+export const cTeacher: Subjects.TeacherInterface = {
+       firstName: 'Ginja',
+       lastName: 'Patty,
+       experienceTeachingC: 10
+}
+console.log('C++');
+cpp.teacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
