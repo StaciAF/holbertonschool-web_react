0789aa0214807c91c9e2d0fe7a8632dbd8d1f97e
StaciAF <aaen.it19@gmail.com>
2021-07-07 15:34:49 -0500

    adds updated structure and files for task_2 completion

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import logo from './holberton-logo.png';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr className="HR-red"></hr>
+      <body className="App-body">
+        <p className="Body-p">Login to access the full dashboard</p>
+        <form>
+          <label>Email:
+            <input type="text" name="email" />
+          </label>
+          <label>Password:
+            <input type="text" name="password" />
+          </label>
+          <button>OK</button>
+        </form>
+      </body>
+      <hr className="HR-red"></hr>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
