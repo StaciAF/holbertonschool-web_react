20cf7ce5660499f1c136fd8e777d3f2b2aab3528
StaciAF <aaen.it19@gmail.com>
2021-07-07 19:35:24 -0500

    adds tests for App and Notification components

--- 0x02-react_intro/task_3/dashboard/src/App.test.js
+++ 0x02-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App />', () => {
+  it('Successfully renders App-header in <App /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).matchesElement(<header className="App-header"><h1>School dashboard</h1></header>).to.equal(true);
+  });
+  it('Successfully renders App-body in <App /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).matchesElement(<div className="App-body"></div>).to.equal(true);
+  });
+  it('Successfully renders App-footer in <App /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).matchesElement(<footer className="App-footer"></footer>).to.equal(true);
+  });
 });
