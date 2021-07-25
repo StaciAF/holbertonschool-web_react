6da9b8e7b68013b970b4e65ba9dcbe53ac5d8047
StaciAF <aaen.it19@gmail.com>
2021-07-25 13:31:54 -0500

    adds updated files

--- 0x03-react_props/task_1/dashboard/src/Header/Header.js
+++ 0x03-react_props/task_1/dashboard/src/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import logo from '../assets/holberton-logo.jpg';
+import logo from '../assets/holberton-logo.jpeg';
 
 const Header = () => {
     return (
