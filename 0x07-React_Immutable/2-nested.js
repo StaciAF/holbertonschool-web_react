83542a8df59ea3545ccd01ab4b2af83bf3c88492
StaciAF <aaen.it19@gmail.com>
2021-07-30 17:00:12 +0000

    removes export module // wrong function call

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -3,5 +3,3 @@ import { getIn, fromJS } from 'immutable';
 export default function accessImmutableObject(object, array) {
     return getIn(fromJS(object), array);
 }
-
-module.exports = accessImmutableObject;
